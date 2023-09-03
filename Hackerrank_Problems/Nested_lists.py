74d8675e54cfce1c5e310d488dbd15d87c109b69
Ajeeth_hari <ajeethaj99@gmail.com>
2023-09-03 22:58:00 +0530

    Update Nested_lists.py

--- Hackerrank_Problems/Nested_lists.py
+++ Hackerrank_Problems/Nested_lists.py
@@ -1,1 +1,9 @@
 
+# Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+# Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+print(list([i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1)  if i+j+k !=n))
