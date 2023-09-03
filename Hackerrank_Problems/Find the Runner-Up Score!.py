2f6f9c29ebfda28fda542cbea9e0f73453db8e15
Ajeeth_hari <ajeethaj99@gmail.com>
2023-09-03 23:06:06 +0530

    Update Find the Runner-Up Score!.py

--- Hackerrank_Problems/Find the Runner-Up Score!.py
+++ Hackerrank_Problems/Find the Runner-Up Score!.py
@@ -1,1 +1,12 @@
 
+#Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. 
+#You are given n scores. Store them in a list and find the score of the runner-up.
+
+n = int(input())
+arr = map(int, input().split())
+arr1 = list(arr)
+nos = []
+for i in arr1:
+    if i < max(arr1):
+        nos.append(i)
+print(max(nos))
