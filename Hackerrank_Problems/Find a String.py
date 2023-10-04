cf081f993961ff1d5dbca43b81eedcc42f87cca6
Ajeeth_hari <ajeethaj99@gmail.com>
2023-10-04 21:02:54 +0530

    Create Find a String.py

---
+++ Hackerrank_Problems/Find a String.py
@@ -0,0 +1,19 @@
+# In this challenge, the user enters a string and a substring. 
+# You have to print the number of times that the substring occurs in the given string. 
+# String traversal will take place from left to right, not from right to left.
+
+# Sample Input
+# ABCDCDC
+# CDC
+
+# Sample Output
+# 2
+
+# Solution: 
+
+def count_substring(string, sub_string):
+    total = 0
+    for i in range(len(string)):
+        if string[i:len(string)].startswith(sub_string):
+            total += 1
+    return total
